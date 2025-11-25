8806a6da28833ad082204ac63b61e157ce8d06a8
Juan Diego <124622362+Jtenjo7@users.noreply.github.com>
2025-11-24 23:26:18 -0500

    Update conio.h

--- conio.h
+++ conio.h
@@ -10,7 +10,15 @@
 
 enum { RESET, BLACK = 30, RED, GREEN, YELLOW, BLUE, PURPLE, CYAN, WHITE };
 
-
+#define COLOR_RESET   "\033[0m"
+#define COLOR_BLACK   "\033[1;30m"
+#define COLOR_RED     "\033[1;31m"
+#define COLOR_GREEN   "\033[1;32m"
+#define COLOR_YELLOW  "\033[1;33m"
+#define COLOR_BLUE    "\033[1;34m"
+#define COLOR_PURPLE  "\033[1;35m"
+#define COLOR_CYAN    "\033[1;36m"
+#define COLOR_WHITE   "\033[1;37m"
 
 /*********************** Prototypes of Public Functions ***********************/
 
